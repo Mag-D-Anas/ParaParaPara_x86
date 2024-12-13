763cbaab62ac8e301726b7e4df945fc1eec9ff49
Amr Samy <41027803+AmrSamy59@users.noreply.github.com>
2024-12-13 03:35:47 +0200

    feat: add paddle logic with movement and drawing functionality, Add Ball bouncing on paddle logic, fix a small issue in UPDATE_POSITION

--- main.asm
+++ main.asm
@@ -3,14 +3,18 @@ extrn MOVE_BALL:FAR
 extrn CLEAR_BALL:FAR
 extrn UPDATE_POSITION:FAR
 extrn DRAW_BALL:FAR
+extrn INIT_BALL:FAR
+; Paddle
+extrn CheckInput:FAR
+extrn ClearPaddle:FAR
+extrn DrawPaddle:FAR
+extrn InitPaddle:FAR
 
 .model small
 .stack 100h
 
 .data
       PREV_MS         DB      0       ; neede for fps movements
-
-
 .code
   MAIN PROC FAR
       mov AX, @DATA
@@ -20,15 +24,19 @@ extrn DRAW_BALL:FAR
       mov      al, 13h                ; Chosen video mode
       INT      10h                    ; Execute interrupt
 
-      CALL    DrawBricks_proc        ; Draw the bricks
+      CALL     InitPaddle              ; Initialize the paddle position
+      CALL     DrawBricks_proc        ; Draw the bricks
+      CALL     INIT_BALL
 
       time_loop:
+            CALL     CheckInput   ; Check for user input
+            CALL     ClearPaddle  ; Erase the old paddle
+            CALL     DrawPaddle   ; Draw the new paddle
             MOV      AH, 2Ch                ; Get the system time
             INT      21h                    ; CH = hours, CL = minutes, DH = seconds, DL = 1/100 seconds
             CMP      PREV_MS, DL            ; Compare the curr ms with the previous one
             JE       time_loop              ; if equal then hold the program 1ms 
             MOV      PREV_MS, DL            ; Update timne
-
             
             CALL     MOVE_BALL              ; check Collisions (for now, the walls only)
             CALL     CLEAR_BALL             ; Erase the ball to draw it in new position
