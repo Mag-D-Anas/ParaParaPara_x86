78ad3a4e342776da99860a8ca5fbee01cec96683
Amr Samy <41027803+AmrSamy59@users.noreply.github.com>
2024-12-24 05:17:37 +0200

    feat: add communication functionality for multiplayer support

--- main.asm
+++ main.asm
@@ -1,11 +1,22 @@
+; Bricks 1
 extrn DrawBricks_proc:FAR
 extrn CheckCollision_proc:FAR
 
+; Bricks 2
+extrn DrawBricks_proc2:FAR
+extrn CheckCollision_proc2:FAR
+
+; Ball 1
 extrn MOVE_BALL:FAR
 extrn CLEAR_BALL:FAR
 extrn UPDATE_POSITION:FAR
 extrn DRAW_BALL:FAR
 extrn INIT_BALL:FAR
+
+; Ball 2
+extrn CLEAR_BALL_REC:FAR
+extrn DRAW_BALL_REC:FAR
+extrn INIT_BALL_REC:FAR
 
 ; Paddle
 extrn CheckInput:FAR
@@ -18,10 +29,12 @@ extrn ClearPaddle2:FAR
 extrn DrawPaddle2:FAR
 extrn InitPaddle2:FAR
 
+; Communication
+extrn COM_INIT:FAR
+extrn SendCom:FAR
+extrn RecCom:FAR
 
-extrn CLEAR_BALL_REC:FAR
-extrn DRAW_BALL_REC:FAR
-extrn INIT_BALL_REC:FAR
+
 
 .model small
 .stack 100h
@@ -40,13 +53,16 @@ extrn INIT_BALL_REC:FAR
       mov      ah, 00h                ; Set the config to video mode
       mov      al, 13h                ; Chosen video mode
       INT      10h                    ; Execute interrupt
-      CALL     DrawVerticalLine_proc
       CALL     InitPaddle              ; Initialize the paddle position
       CALL     InitPaddle2             ; Initialize the paddle position
       CALL     DrawBricks_proc        ; Draw the bricks
+      CALL     DrawBricks_proc2        ; Draw the bricks
       CALL     INIT_BALL
       CALL     INIT_BALL_REC
+      CALL     COM_INIT
+
       time_loop:
+            CALL     DrawVerticalLine_proc
             ; Paddle 1
             CALL     CheckInput   ; Check for user input
             CALL     ClearPaddle2  ; Erase the old paddle
@@ -55,7 +71,8 @@ extrn INIT_BALL_REC:FAR
             ; Paddle 2
             ; CALL     CheckInput2   ; Check for user input
               
-              CALL     DrawPaddle2   ; Draw the new paddle
+            CALL     DrawPaddle2   ; Draw the new paddle
+
 
             MOV      AH, 2Ch                ; Get the system time
             INT      21h                    ; CH = hours, CL = minutes, DH = seconds, DL = 1/100 seconds
@@ -63,14 +80,19 @@ extrn INIT_BALL_REC:FAR
             JE       time_loop              ; if equal then hold the program 1ms 
             MOV      PREV_MS, DL            ; Update timne
             
+            CALL     SendCom
+
             CALL     MOVE_BALL              ; check Collisions (for now, the walls only)
             CALL     CLEAR_BALL             ; Erase the ball to draw it in new position
             CALL     CLEAR_BALL_REC
             CALL     CheckCollision_proc    ; check bricks collision
+            CALL     RecCom
+            CALL     CheckCollision_proc2   ; check bricks collision
             CALL     UPDATE_POSITION        ; update position of the ball
             CALL     DRAW_BALL_REC
             CALL     DRAW_BALL              ; Draw the ball with moved ( X - Y ) initial position
-            
+
+
             JMP      time_loop              ; REPEAT TO INFINITY
     EXITPROG:      
             mov      ah, 4Ch
